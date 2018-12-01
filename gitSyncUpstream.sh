93bc7ce2364c1e016f0cd15deb08d66746242fba
Ahmong <ahmong@126.com>
2018-12-01 18:44:29 +0800

    Shell script for sync origin with upstream

---
+++ gitSyncUpstream.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#
+#Run the following cmd once at the first time.
+#
+# git remote add upstream git://github.com/XXX/xxx
+
+git fetch upstream
+
+BRANCHES="master compose-v2 framework gh-pages LaraDock-ToolBox"
+
+getStables()
+{
+  for STR in MOODLE_{19..35}_STABLE; do
+    BRANCHES="$BRANCHES $STR"
+  done
+}
+
+for BRANCH in $BRANCHES; do
+    echo "Pushing..." $BRANCH
+    git push origin refs/remotes/upstream/$BRANCH:refs/heads/$BRANCH
+    echo -e "...Finished($BRANCH)\n"
+done
